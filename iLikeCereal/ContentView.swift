fcfb59c05f9200ada937c0015805134ffb1da98e
Logan Koshenka <koshenka27@gmail.com>
2021-08-14 19:05:33 -0400

    Initial Commit

---
+++ iLikeCereal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iLikeCereal
+//
+//  Created by Logan Koshenka on 8/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iLikeCereal/iLikeCerealApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLikeCerealApp.swift
+//  iLikeCereal
+//
+//  Created by Logan Koshenka on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct iLikeCerealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
